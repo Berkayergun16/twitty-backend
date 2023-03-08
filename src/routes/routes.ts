7bd340a1a21637de53b0083a57c0601739633742
Berkay <berkay.ergun16@gmail.com>
2023-03-08 23:39:02 +0300

    added jwt token control

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
-import express, {Express,Request,Response} from 'express';
+import express from 'express';
 import usersRouter from './usersRouter';
+import authRouter from './authRouter';
 const router = express.Router();
 
 router.use('/users',usersRouter);
+router.use('/auth', authRouter);
 
 
 export default router;
